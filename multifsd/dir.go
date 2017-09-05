f0c7973506da38a8f5fb9098534ad6b95b0eb91c
jiayaohuang <jiayaohuang@pptv.com>
2017-09-05 19:24:18 +0800

    Dir.ReadDirAll: Fix dir tree relationship

--- multifsd/dir.go
+++ multifsd/dir.go
@@ -21,46 +21,26 @@ type Dir struct {
 // Attr for get attr of directory.
 func (dir *Dir) Attr(ctx context.Context, a *fuse.Attr) error {
 	//log.Println("Dir: Attr:", dir.Path)
-	var (
-	//p    *Dir
-	//pTmp *fs.Node
-	//err error
-	)
-	//err = nil
-	if dir.Path == "/" {
-		a.Inode = 1
-		if fusefs.readOnly == true {
-			a.Mode = os.ModeDir | 0555
-		} else {
-			a.Mode = os.ModeDir | 0775
-		}
+	var ()
+	pFInfo, _ := os.Stat(filepath.Dir(filepath.Join(fusefs.target, dir.Path)))
+	sysStat, ok := pFInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return errors.New("Not a syscall.Stat_t")
+	}
+	a.Inode = fs.GenerateDynamicInode(sysStat.Ino,
+		filepath.Base(dir.Path))
+	// TODO: get file mode from backend
+	if fusefs.readOnly == true {
+		a.Mode = os.ModeDir | 0555
 	} else {
-		//*pTmp, err = dir.Lookup(ctx, "..")
-		//if err != nil {
-		//	return err
-		//}
-		//p = pTmp.(Dir)
-		//pFInfo, _ := os.Stat(filepath.Join(fusefs.target, p.Path))
-		pFInfo, _ := os.Stat(filepath.Dir(filepath.Join(fusefs.target, dir.Path)))
-		sysStat, ok := pFInfo.Sys().(*syscall.Stat_t)
-		if !ok {
-			return errors.New("Not a syscall.Stat_t")
-		}
-		a.Inode = fs.GenerateDynamicInode(sysStat.Ino,
-			filepath.Base(dir.Path))
-		// TODO: get file mode from backend
-		if fusefs.readOnly == true {
-			a.Mode = os.ModeDir | 0555
-		} else {
-			a.Mode = os.ModeDir | 0775
-		}
+		a.Mode = os.ModeDir | 0775
 	}
 	return nil
 }
 
 // Lookup return the sub Node in this directory.
 func (dir *Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
-	//log.Println("Dir: Lookup:", nodePath)
+	log.Println("Dir: Lookup:", name)
 	nodePath := filepath.Join(dir.Path, name)
 
 	// Lookup from master
@@ -100,22 +80,29 @@ func (dir *Dir) getDirent(fInfo os.FileInfo) (fuse.Dirent, error) {
 
 // ReadDirAll function read the all entry of this directory.
 func (dir *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
-	//log.Println("Dir: ReadDirAll:", dir.Path)
+	log.Println("Dir: ReadDirAll:", dir.Path)
 	var (
-		dirmap  map[string]fuse.Dirent
-		dirents []fuse.Dirent
+		dirmap     map[string]fuse.Dirent
+		dirents    []fuse.Dirent
+		dirAbsPath string
+		allerr     error
 	)
 
 	dirmap = make(map[string]fuse.Dirent)
+	dirAbsPath = filepath.Join(fusefs.target, dir.Path)
+
+	// Add default entry
+	dirmap[dirAbsPath] = fuse.Dirent{Name: "."}
+	dirmap[filepath.Dir(dirAbsPath)] = fuse.Dirent{Name: ".."}
 
 	// Read from master
 	mEntInfos, err := ioutil.
 		ReadDir(filepath.Join(fusefs.master, dir.Path))
 	if err != nil {
-		return nil, err
+		allerr = err
 	}
 	for _, mEntInfo := range mEntInfos {
-		entpath := filepath.Join(dir.Path, mEntInfo.Name())
+		entpath := filepath.Join(dirAbsPath, mEntInfo.Name())
 		dirmap[entpath] = fuse.Dirent{Name: mEntInfo.Name()}
 	}
 
@@ -123,12 +110,12 @@ func (dir *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 	for _, slave := range fusefs.slaves {
 		sEntInfos, err := ioutil.
 			ReadDir(filepath.Join(slave, dir.Path))
-		if err != nil {
-			return nil, err
+		if err == nil {
+			allerr = nil
 		}
 	GetSlaves:
 		for _, sEntInfo := range sEntInfos {
-			entpath := filepath.Join(dir.Path, sEntInfo.Name())
+			entpath := filepath.Join(dirAbsPath, sEntInfo.Name())
 			if _, ok := dirmap[entpath]; ok {
 				continue GetSlaves
 			}
@@ -140,5 +127,8 @@ func (dir *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 		dirents = append(dirents, ent)
 	}
 
+	if allerr != nil {
+		return nil, allerr
+	}
 	return dirents, nil
 }
